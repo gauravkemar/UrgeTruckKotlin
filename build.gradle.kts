e07af2336af4cd0cde6d858684eacdff796b4f77
GB <testspi10123@gmail.com>
2024-05-21 12:36:30 +0530

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,17 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+    val agp_version by extra("7.3.1")
+    repositories {
+        google()
+        mavenCentral()
+        maven(url = "https://jitpack.io")
+    }
+    dependencies {
+        val nav_version = "2.5.2"
+        classpath ("androidx.navigation:navigation-safe-args-gradle-plugin:$nav_version")
+    }
+}
+plugins {
+    id("com.android.application") version "8.1.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "UrgeTruckKotlin"
+include(":app")
+include (":RFIDAPI3Library")
